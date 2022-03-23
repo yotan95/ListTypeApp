48601d2fa78992ad6db9fbddcef6f5629d12fbe8
yodong <jeongs741@naver.com>
2022-03-23 16:23:33 +0900

    init

---
+++ app/src/main/java/com/practice/listtypeapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.practice.listtypeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+/*
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yotan95/ListTypeApp.git
+git push -u origin main
+*/
