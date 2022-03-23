1b6fe6c5da2a6d8e6b88b16bf9563b89a10fba5f
yodong <jeongs741@naver.com>
2022-03-23 16:24:09 +0900

    init

--- app/src/main/java/com/practice/listtypeapp/MainActivity.java
+++ app/src/main/java/com/practice/listtypeapp/MainActivity.java
@@ -12,11 +12,3 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
-/*
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/yotan95/ListTypeApp.git
-git push -u origin main
-*/
